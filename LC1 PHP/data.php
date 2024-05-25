1a77b3831c23d120857cec2fe8d7aa54fe80c170
TalibMari <46760536+TalibMari@users.noreply.github.com>
2024-05-25 20:51:44 +0500

    LC1 25/05/2024
    Intro ABOUT PHP 
    How To Use HTML CSS BOOTSTRAP AND JS
    Variables

---
+++ LC1 PHP/data.php
@@ -0,0 +1,25 @@
+<?php 
+// ECHO "Hello World";      // Statment
+// pRint "Hello World";
+
+echo "<h1>Heading 1</h1>";
+// echo "<script>
+//     alert('JS Working')
+// </script>";
+?>
+
+<table border="1">
+    <th>Name</th>
+    <th>Email</th>
+    <tr>
+        <td>Abc</td>
+        <td>Abc@gmail.com</td>
+    </tr>
+
+</table>
+
+
+
+<script>
+    alert("JS Working")
+</script>
---
+++ LC1 PHP/test.php
@@ -0,0 +1,24 @@
+<?php 
+
+// $name = "Abc";
+// $name= "Xyz";
+// $age = 20;
+
+// // echo $name.$age;
+
+// echo "Student Name ".$name;
+// echo "<br>Student Age ".$age;
+
+
+$a = 10;
+$b = 50;
+$name = "Abc";
+$bool = true;
+
+
+var_dump($bool);
+
+// echo "Sum Is ".($a+$b);
+
+
+?>
